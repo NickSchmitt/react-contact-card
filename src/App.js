32dc1e329eceee308b6826f81826c758d0825936
nickschmitt <nickschmitt@gmail.com>
2020-10-22 19:38:35 -0700

    added components props; avatar, name, email, age;

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles.css'
 
 const App = () => {
   return (
     <>
-      <ContactCard />
-      <ContactCard />
-      <ContactCard />
+      <ContactCard
+        avatar='https://via.placeholder.com/150'
+        name='Jenny Han'
+        email='jenny.han@notreal.com'
+        age={25}
+      />
+
+      <ContactCard
+        avatar='https://via.placeholder.com/150'
+        name='Jason Long'
+        email='jason.long@notreal.com'
+        age={45}
+      />
+
+      <ContactCard
+        avatar='https://via.placeholder.com/150'
+        name='Peter Pan'
+        email='peter.pan@neverland.com'
+        age={100}
+      />
     </>
   )
 }
 
-const ContactCard = () => {
+const ContactCard = (props) => {
+  const [showAge, setShowAge] = useState(false)
+
   return (
     <div className='contact-card'>
-      <img src='https://via.placeholder.com/150' alt='profile' />
+      <img src={props.avatar} alt='profile' />
       <div className='user-details'>
-        <p>Name: Jenny Han</p>
-        <p>Email: Jenny.Han@notreal.com</p>
-        <p>Age: 25</p>
+        <p>Name: {props.name}</p>
+        <p>Email: {props.email}</p>
+        <button onClick={() => setShowAge(!showAge)}>Toggle Age </button>
+        {showAge && <p>Age: {props.age}</p>}
       </div>
     </div>
   )
